15ff5ee514f94fda2a960dd69d5b30ee9cdd1da1
Senya <senya.korotkov@gmail.com>
2020-07-01 14:21:19 +0300

    Week three first task

---
+++ week3/first_task.py
@@ -0,0 +1,125 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractEffect(ABC, Hero):
+    def __init__(self, base):
+        super().__init__()
+        self.base = base
+
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+    @abstractmethod
+    def get_stats(self):
+        return self.base.get_stats()
+
+
+class AbstractPositive(AbstractEffect):
+    def __init__(self, base):
+        super().__init__(base)
+        self.base = base
+
+    @abstractmethod
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_stats(self):
+        return self.base.get_stats()
+
+
+class AbstractNegative(AbstractEffect):
+    def __init__(self, base):
+        super().__init__(base)
+        self.base = base
+
+    @abstractmethod
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+    def get_stats(self):
+        return self.base.get_stats()
+
+
+class Berserk(AbstractPositive):
+    def get_positive_effects(self):
+        positive_effects = self.base.get_positive_effects()
+        positive_effects.append('Berserk')
+        return positive_effects
+
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['HP'] += 50
+        stats['Strength'] += 7
+        stats['Perception'] -= 3
+        stats['Endurance'] += 7
+        stats['Charisma'] -= 3
+        stats['Intelligence'] -= 3
+        stats['Agility'] += 7
+        stats['Luck'] += 7
+        return stats
+
+
+class Blessing(AbstractPositive):
+    def get_positive_effects(self):
+        positive_effects = self.base.get_positive_effects()
+        positive_effects.append('Blessing')
+        return positive_effects
+
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] += 2
+        stats['Perception'] += 2
+        stats['Endurance'] += 2
+        stats['Charisma'] += 2
+        stats['Intelligence'] += 2
+        stats['Agility'] += 2
+        stats['Luck'] += 2
+        return stats
+
+
+class Weakness(AbstractNegative):
+    def get_negative_effects(self):
+        negative_effects = self.base.get_negative_effects()
+        negative_effects.append('Weakness')
+        return negative_effects
+
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] -= 4
+        stats['Endurance'] -= 4
+        stats['Agility'] -= 4
+        return stats
+
+
+class EvilEye(AbstractNegative):
+    def get_negative_effects(self):
+        negative_effects = self.base.get_negative_effects()
+        negative_effects.append('EvilEye')
+        return negative_effects
+
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Luck'] -= 10
+        return stats
+
+
+class Curse(AbstractNegative):
+    def get_negative_effects(self):
+        negative_effects = self.base.get_negative_effects()
+        negative_effects.append('Curse')
+        return negative_effects
+
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] -= 2
+        stats['Perception'] -= 2
+        stats['Endurance'] -= 2
+        stats['Charisma'] -= 2
+        stats['Intelligence'] -= 2
+        stats['Agility'] -= 2
+        stats['Luck'] -= 2
+        return stats
+
